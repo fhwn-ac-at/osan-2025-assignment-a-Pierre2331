de5e6802401ba4426b8c61c4ef7edc165a0dcfd2
Pierre Erhardt <209910@fhwn.ac.at>
2025-04-10 10:17:31 +0200

    Erster Versuch Mitarbeit :)

--- pfusch.c
+++ pfusch.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
 
-int main(void) {
-    printf("Hello, Kevin!\n");
+typedef struct command_line_arguments {
+    int i;
+    char const *s;
+    bool b;
+} cli_args;
+
+cli_args parse_command_line(int const argc, char * argv[]) {
+    cli_args args = {0, NULL, false};
+
+    int optgot = -1;
+
+    do {
+        optgot = getopt(argc, argv, "i:s:b");
+        switch (optgot)
+        {
+        case 'i':
+            args.i = atoi(optarg);
+            break;
+        case 's':
+            args.s = optarg;
+            break;
+        case 'b':
+           args.b = true;
+            break;
+
+        case '?':
+            exit(EXIT_FAILURE);
+        }
+    } while (optgot != -1);
+
+    return args;
+}
+
+int main(int argc, char * argv[]) {
+
+    
+    
+    //printf("gotopt: %c\n", gotopt);
+
+    cli_args const args = parse_command_line(argc, argv);
+    printf("i = %d, s: %s, b: %d\n", args.i, args.s, args.b);
 
     return 0;
 }
